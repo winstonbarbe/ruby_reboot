29c2547a368905c7fa77f8bbdd708d2ed88f11b3
winstonbarbe <winston.barbe@gmail.com>
2022-01-21 16:48:17 -0500

    begins to solve logic for deck

--- go_fish.rb
+++ go_fish.rb
@@ -7,4 +7,41 @@
 # deal clockwise
 # the player can request any card they have from any other player also knonw as fishing
 # if the requestee has any matching cards then they must hand them all over. 
-# as long as the player fishing requests is successful they continue. 
+# as long as the player fishing requests is successful they continue.
+
+class Deck
+  def initialize
+    @suits = {
+      hearts: [],
+      spades: [],
+      clubs: [],
+      diamonds: []
+    }
+    
+    @values = {
+      two: 2,
+      three: 3,
+      four: 4,
+      five: 5,
+      six: 6,
+      seven: 7,
+      eight: 8,
+      nine: 9,
+      ten: 10,
+      jack: 11,
+      queen: 12,
+      king: 13,
+      ace: 14,
+    }
+
+
+    @shuffle = @suits
+  end
+  def shuffle
+    @shuffle
+  end
+  
+end
+
+deck = Deck.new
+p deck.shuffle
