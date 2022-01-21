23dc4db9fc616215a93affeb915129a2c3240436
winstonbarbe <winston.barbe@gmail.com>
2022-01-21 16:16:01 -0500

    defines rules for game of go fish; partial

---
+++ go_fish.rb
@@ -0,0 +1,10 @@
+# 52 card deck
+# the goal of the game is to get as many pairs as possible
+# 2 - 5 players
+# 2 - 3 player games, deal 7 cards each
+# 4 - 5  player games, deal 5 cards each
+# ace is high / two is low
+# deal clockwise
+# the player can request any card they have from any other player also knonw as fishing
+# if the requestee has any matching cards then they must hand them all over. 
+# as long as the player fishing requests is successful they continue. 
